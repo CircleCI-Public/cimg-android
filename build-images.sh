a791fefbb434e14b1fdf54bc437512ac57e940c5
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-05-28 13:30:41 -0400

    Add newer NDK version [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 30.0/Dockerfile -t cimg/android:30.0  -t cimg/android:priceline .
-docker build --file 29.0/Dockerfile -t cimg/android:29.0  -t cimg/android:priceline .
-docker build --file 28.0/Dockerfile -t cimg/android:28.0  -t cimg/android:priceline .
-docker build --file 27.0/Dockerfile -t cimg/android:27.0  -t cimg/android:priceline .
+docker build --file 30.0/Dockerfile -t cimg/android:30.0 .
+docker build --file 29.0/Dockerfile -t cimg/android:29.0 .
+docker build --file 28.0/Dockerfile -t cimg/android:28.0 .
+docker build --file 27.0/Dockerfile -t cimg/android:27.0 .
