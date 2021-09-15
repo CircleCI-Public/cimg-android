8f9bd44f8fd61827df69755825d9038ac376193c
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-09-14 22:53:02 -0400

    Publish v2021.09.1. [release] (#18)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 2021.08/Dockerfile -t cimg/android:2021.08.1  -t cimg/android:2021.08 .
-docker build --file 2021.08/ndk/Dockerfile -t cimg/android:2021.08.1-ndk  -t cimg/android:2021.08-ndk .
+docker build --file 2021.09/Dockerfile -t cimg/android:2021.09.1  -t cimg/android:2021.09 .
+docker build --file 2021.09/ndk/Dockerfile -t cimg/android:2021.09.1-ndk  -t cimg/android:2021.09-ndk .
