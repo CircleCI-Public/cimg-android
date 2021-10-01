a1d3b0335ceb1348e0465c18bcbeb76650b3cc74
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-10-01 17:20:49 -0400

    Publish v2021.10.1. [release] (#23)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 2021.09/Dockerfile -t cimg/android:2021.09.2  -t cimg/android:2021.09 .
-docker build --file 2021.09/ndk/Dockerfile -t cimg/android:2021.09.2-ndk  -t cimg/android:2021.09-ndk .
-docker build --file 2021.09/node/Dockerfile -t cimg/android:2021.09.2-node  -t cimg/android:2021.09-node .
-docker build --file 2021.09/browsers/Dockerfile -t cimg/android:2021.09.2-browsers  -t cimg/android:2021.09-browsers .
+docker build --file 2021.10/Dockerfile -t cimg/android:2021.10.1  -t cimg/android:2021.10 .
+docker build --file 2021.10/ndk/Dockerfile -t cimg/android:2021.10.1-ndk  -t cimg/android:2021.10-ndk .
+docker build --file 2021.10/node/Dockerfile -t cimg/android:2021.10.1-node  -t cimg/android:2021.10-node .
+docker build --file 2021.10/browsers/Dockerfile -t cimg/android:2021.10.1-browsers  -t cimg/android:2021.10-browsers .
