6714968643fda0600301bf999200c4df6dc22344
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-10-20 21:26:26 -0400

    Pre-install system Ruby & Bundler [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 2021.10/Dockerfile -t cimg/android:2021.10.1  -t cimg/android:2021.10 .
-docker build --file 2021.10/ndk/Dockerfile -t cimg/android:2021.10.1-ndk  -t cimg/android:2021.10-ndk .
-docker build --file 2021.10/node/Dockerfile -t cimg/android:2021.10.1-node  -t cimg/android:2021.10-node .
-docker build --file 2021.10/browsers/Dockerfile -t cimg/android:2021.10.1-browsers  -t cimg/android:2021.10-browsers .
+docker build --file 2021.10/Dockerfile -t cimg/android:2021.10.2  -t cimg/android:2021.10 .
+docker build --file 2021.10/ndk/Dockerfile -t cimg/android:2021.10.2-ndk  -t cimg/android:2021.10-ndk .
+docker build --file 2021.10/node/Dockerfile -t cimg/android:2021.10.2-node  -t cimg/android:2021.10-node .
+docker build --file 2021.10/browsers/Dockerfile -t cimg/android:2021.10.2-browsers  -t cimg/android:2021.10-browsers .
