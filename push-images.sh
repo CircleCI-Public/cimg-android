c7b609655c79682118589071bb3c3190cbffadde
Jeff Chen <jeff.chen@circleci.com>
2022-01-27 08:54:48 -0800

    Publish v2022.01.1. [release]

---
+++ push-images.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker push cimg/android:2022.01.1
+docker push cimg/android:2022.01
+docker push cimg/android:2022.01.1-ndk
+docker push cimg/android:2022.01-ndk
+docker push cimg/android:2022.01.1-node
+docker push cimg/android:2022.01-node
+docker push cimg/android:2022.01.1-browsers
+docker push cimg/android:2022.01-browsers
