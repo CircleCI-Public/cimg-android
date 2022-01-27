8fadf51b3f537ad5e268cda496cd57c2c5375458
Jalexchen <jeff.chen@circleci.com>
2022-01-27 09:03:24 -0800

    Publish v2022.01.1. [release] (#38)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2021.12/Dockerfile -t cimg/android:2021.12.2  -t cimg/android:2021.12 .
-docker build --file 2021.12/ndk/Dockerfile -t cimg/android:2021.12.2-ndk  -t cimg/android:2021.12-ndk .
-docker build --file 2021.12/node/Dockerfile -t cimg/android:2021.12.2-node  -t cimg/android:2021.12-node .
-docker build --file 2021.12/browsers/Dockerfile -t cimg/android:2021.12.2-browsers  -t cimg/android:2021.12-browsers .
+docker build --file 2022.01/Dockerfile -t cimg/android:2022.01.1  -t cimg/android:2022.01 .
+docker build --file 2022.01/ndk/Dockerfile -t cimg/android:2022.01.1-ndk  -t cimg/android:2022.01-ndk .
+docker build --file 2022.01/node/Dockerfile -t cimg/android:2022.01.1-node  -t cimg/android:2022.01-node .
+docker build --file 2022.01/browsers/Dockerfile -t cimg/android:2022.01.1-browsers  -t cimg/android:2022.01-browsers .
