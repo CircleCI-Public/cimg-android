7af5c262821e4bf68c19cdb0ed34283165684e11
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-03-07 11:11:34 -0500

    Android March 2022 [release] (#40)

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.01/Dockerfile -t cimg/android:2022.01.1  -t cimg/android:2022.01 .
-docker build --file 2022.01/ndk/Dockerfile -t cimg/android:2022.01.1-ndk  -t cimg/android:2022.01-ndk .
-docker build --file 2022.01/node/Dockerfile -t cimg/android:2022.01.1-node  -t cimg/android:2022.01-node .
-docker build --file 2022.01/browsers/Dockerfile -t cimg/android:2022.01.1-browsers  -t cimg/android:2022.01-browsers .
+docker build --file 2022.03/Dockerfile -t cimg/android:2022.03.1  -t cimg/android:2022.03 .
+docker build --file 2022.03/ndk/Dockerfile -t cimg/android:2022.03.1-ndk  -t cimg/android:2022.03-ndk .
+docker build --file 2022.03/node/Dockerfile -t cimg/android:2022.03.1-node  -t cimg/android:2022.03-node .
+docker build --file 2022.03/browsers/Dockerfile -t cimg/android:2022.03.1-browsers  -t cimg/android:2022.03-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/android:2022.01.1
-docker push cimg/android:2022.01
-docker push cimg/android:2022.01.1-ndk
-docker push cimg/android:2022.01-ndk
-docker push cimg/android:2022.01.1-node
-docker push cimg/android:2022.01-node
-docker push cimg/android:2022.01.1-browsers
-docker push cimg/android:2022.01-browsers
+docker push cimg/android:2022.03.1
+docker push cimg/android:2022.03
+docker push cimg/android:2022.03.1-ndk
+docker push cimg/android:2022.03-ndk
+docker push cimg/android:2022.03.1-node
+docker push cimg/android:2022.03-node
+docker push cimg/android:2022.03.1-browsers
+docker push cimg/android:2022.03-browsers
