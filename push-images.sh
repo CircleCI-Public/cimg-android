fd3f49206d85401cc33853f780e73bb5ff6331d7
Jalexchen <jeff.chen@circleci.com>
2022-08-01 13:05:47 -0700

    july cimg-android update [release] (#53)
    * july cimg-android update
    
    * publish 2022.07.1 [release]

--- push-images.sh
+++ push-images.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-
-docker push cimg/android:2022.06.1
-docker push cimg/android:2022.06
-docker push cimg/android:2022.06.1-ndk
-docker push cimg/android:2022.06-ndk
-docker push cimg/android:2022.06.1-node
-docker push cimg/android:2022.06-node
-docker push cimg/android:2022.06.1-browsers
-docker push cimg/android:2022.06-browsers
+docker push cimg/android:2022.07
+docker push cimg/android:2022.07.1
+docker push cimg/android:2022.07-ndk
+docker push cimg/android:2022.07.1-ndk
+docker push cimg/android:2022.07-node
+docker push cimg/android:2022.07.1-node
+docker push cimg/android:2022.07-browsers
+docker push cimg/android:2022.07.1-browsers
