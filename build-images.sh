fb1ac43518f7ee7efdbf693c09562ce41c42a0cf
Jeff Chen <jeff.chen@circleci.com>
2022-08-01 10:25:54 -0700

    july cimg-android update

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.06/Dockerfile -t cimg/android:2022.06.1  -t cimg/android:2022.06 .
-docker build --file 2022.06/ndk/Dockerfile -t cimg/android:2022.06.1-ndk  -t cimg/android:2022.06-ndk .
-docker build --file 2022.06/node/Dockerfile -t cimg/android:2022.06.1-node  -t cimg/android:2022.06-node .
-docker build --file 2022.06/browsers/Dockerfile -t cimg/android:2022.06.1-browsers  -t cimg/android:2022.06-browsers .
+docker build --file 2022.07/Dockerfile -t cimg/android:2022.07.1 -t cimg/android:2022.07 .
+docker build --file 2022.07/ndk/Dockerfile -t cimg/android:2022.07.1-ndk -t cimg/android:2022.07-ndk .
+docker build --file 2022.07/node/Dockerfile -t cimg/android:2022.07.1-node -t cimg/android:2022.07-node .
+docker build --file 2022.07/browsers/Dockerfile -t cimg/android:2022.07.1-browsers -t cimg/android:2022.07-browsers .
