d5485bc248eed0421e3c0ee36f7f4047212c0f65
Jalexchen <jeff.chen@circleci.com>
2022-08-10 08:00:45 -0700

    [release] august android update (#55)

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.07/Dockerfile -t cimg/android:2022.07.1 -t cimg/android:2022.07 .
-docker build --file 2022.07/ndk/Dockerfile -t cimg/android:2022.07.1-ndk -t cimg/android:2022.07-ndk .
-docker build --file 2022.07/node/Dockerfile -t cimg/android:2022.07.1-node -t cimg/android:2022.07-node .
-docker build --file 2022.07/browsers/Dockerfile -t cimg/android:2022.07.1-browsers -t cimg/android:2022.07-browsers .
+docker build --file 2022.08/Dockerfile -t cimg/android:2022.08.1 -t cimg/android:2022.08 .
+docker build --file 2022.08/ndk/Dockerfile -t cimg/android:2022.08.1-ndk -t cimg/android:2022.08-ndk .
+docker build --file 2022.08/node/Dockerfile -t cimg/android:2022.08.1-node -t cimg/android:2022.08-node .
+docker build --file 2022.08/browsers/Dockerfile -t cimg/android:2022.08.1-browsers -t cimg/android:2022.08-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/android:2022.07
-docker push cimg/android:2022.07.1
-docker push cimg/android:2022.07-ndk
-docker push cimg/android:2022.07.1-ndk
-docker push cimg/android:2022.07-node
-docker push cimg/android:2022.07.1-node
-docker push cimg/android:2022.07-browsers
-docker push cimg/android:2022.07.1-browsers
+docker push cimg/android:2022.08
+docker push cimg/android:2022.08.1
+docker push cimg/android:2022.08-ndk
+docker push cimg/android:2022.08.1-ndk
+docker push cimg/android:2022.08-node
+docker push cimg/android:2022.08.1-node
+docker push cimg/android:2022.08-browsers
+docker push cimg/android:2022.08.1-browsers
