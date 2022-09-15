9aacbd721f66bc2981f1edcd52b74dfc42186f36
Jeff Chen <jeff.chen@circleci.com>
2022-09-15 09:42:51 -0700

    Publish v2022.09.2. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.09/Dockerfile -t cimg/android:2022.09.1 -t cimg/android:2022.09 .
-docker build --file 2022.09/ndk/Dockerfile -t cimg/android:2022.09.1-ndk -t cimg/android:2022.09-ndk .
-docker build --file 2022.09/node/Dockerfile -t cimg/android:2022.09.1-node -t cimg/android:2022.09-node .
-docker build --file 2022.09/browsers/Dockerfile -t cimg/android:2022.09.1-browsers -t cimg/android:2022.09-browsers .
+docker build --file 2022.09/Dockerfile -t cimg/android:2022.09.2 -t cimg/android:2022.09 .
+docker build --file 2022.09/ndk/Dockerfile -t cimg/android:2022.09.2-ndk -t cimg/android:2022.09-ndk .
+docker build --file 2022.09/node/Dockerfile -t cimg/android:2022.09.2-node -t cimg/android:2022.09-node .
+docker build --file 2022.09/browsers/Dockerfile -t cimg/android:2022.09.2-browsers -t cimg/android:2022.09-browsers .
