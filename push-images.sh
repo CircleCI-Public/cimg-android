64e29157aa22415076a2c160a2a836d4e4168801
Jalexchen <jeff.chen@circleci.com>
2022-09-15 09:52:49 -0700

    Publish v2022.09.2. [release] (#58)

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/android:2022.09
-docker push cimg/android:2022.09.1
+docker push cimg/android:2022.09.2
 docker push cimg/android:2022.09-ndk
-docker push cimg/android:2022.09.1-ndk
+docker push cimg/android:2022.09.2-ndk
 docker push cimg/android:2022.09-node
-docker push cimg/android:2022.09.1-node
+docker push cimg/android:2022.09.2-node
 docker push cimg/android:2022.09-browsers
-docker push cimg/android:2022.09.1-browsers
+docker push cimg/android:2022.09.2-browsers
