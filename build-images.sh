4238ef6550f303ab3641cd2653b79f6fd2c5c176
Jeff Chen <jeff.chen@circleci.com>
2023-02-13 16:15:34 -0800

    Publish v2023.02.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.12/Dockerfile -t cimg/android:2022.12.1 -t cimg/android:2022.12 .
-docker build --file 2022.12/ndk/Dockerfile -t cimg/android:2022.12.1-ndk -t cimg/android:2022.12-ndk .
-docker build --file 2022.12/node/Dockerfile -t cimg/android:2022.12.1-node -t cimg/android:2022.12-node .
-docker build --file 2022.12/browsers/Dockerfile -t cimg/android:2022.12.1-browsers -t cimg/android:2022.12-browsers .
+docker build --file 2023.02/Dockerfile -t cimg/android:2023.02.1 -t cimg/android:2023.02 --platform linux/amd64 .
+docker build --file 2023.02/ndk/Dockerfile -t cimg/android:2023.02.1-ndk -t cimg/android:2023.02-ndk --platform linux/amd64 .
+docker build --file 2023.02/node/Dockerfile -t cimg/android:2023.02.1-node -t cimg/android:2023.02-node --platform linux/amd64 .
+docker build --file 2023.02/browsers/Dockerfile -t cimg/android:2023.02.1-browsers -t cimg/android:2023.02-browsers --platform linux/amd64 .
