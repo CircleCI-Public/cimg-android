3fd63ef233eec54b43754869af11539a3a08442f
ryanbourdais <ryanbourdais@outlook.com>
2023-03-02 12:02:58 -0600

    Publish v2023.03.01 [release]

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.02/Dockerfile -t cimg/android:2023.02.1 -t cimg/android:2023.02 --platform linux/amd64 .
-docker build --file 2023.02/ndk/Dockerfile -t cimg/android:2023.02.1-ndk -t cimg/android:2023.02-ndk --platform linux/amd64 .
-docker build --file 2023.02/node/Dockerfile -t cimg/android:2023.02.1-node -t cimg/android:2023.02-node --platform linux/amd64 .
-docker build --file 2023.02/browsers/Dockerfile -t cimg/android:2023.02.1-browsers -t cimg/android:2023.02-browsers --platform linux/amd64 .
+docker build --file 2023.03/Dockerfile -t cimg/android:2023.03.01 -t cimg/android:2023.03 --platform linux/amd64 .
+docker build --file 2023.03/ndk/Dockerfile -t cimg/android:2023.03.01-ndk -t cimg/android:2023.03-ndk --platform linux/amd64 .
+docker build --file 2023.03/node/Dockerfile -t cimg/android:2023.03.01-node -t cimg/android:2023.03-node --platform linux/amd64 .
+docker build --file 2023.03/browsers/Dockerfile -t cimg/android:2023.03.01-browsers -t cimg/android:2023.03-browsers --platform linux/amd64 .
