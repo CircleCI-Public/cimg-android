33a45bafe9492c15d943ffba2dec11970233c277
ryanbourdais <ryanbourdais@outlook.com>
2023-03-21 13:31:18 -0500

    fix: updated build tools

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/android:2023.03
-docker push cimg/android:2023.03.01
+docker push cimg/android:2023.03.1
 docker push cimg/android:2023.03-ndk
-docker push cimg/android:2023.03.01-ndk
+docker push cimg/android:2023.03.1-ndk
 docker push cimg/android:2023.03-node
-docker push cimg/android:2023.03.01-node
+docker push cimg/android:2023.03.1-node
 docker push cimg/android:2023.03-browsers
-docker push cimg/android:2023.03.01-browsers
+docker push cimg/android:2023.03.1-browsers
