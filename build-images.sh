bfb54ba7d77a7da5ed78ec0004cdbd7a378c29e6
Ryan Bourdais <67397011+ryanbourdais@users.noreply.github.com>
2023-04-04 17:01:03 -0500

    Publish v2023.04.1. [release] (#77)
    * Publish v2023.04.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.03/Dockerfile -t cimg/android:2023.03.1 -t cimg/android:2023.03 --platform linux/amd64 .
-docker build --file 2023.03/ndk/Dockerfile -t cimg/android:2023.03.1-ndk -t cimg/android:2023.03-ndk --platform linux/amd64 .
-docker build --file 2023.03/node/Dockerfile -t cimg/android:2023.03.1-node -t cimg/android:2023.03-node --platform linux/amd64 .
-docker build --file 2023.03/browsers/Dockerfile -t cimg/android:2023.03.1-browsers -t cimg/android:2023.03-browsers --platform linux/amd64 .
+docker build --file 2023.04/Dockerfile -t cimg/android:2023.04.1 -t cimg/android:2023.04 --platform linux/amd64 .
+docker build --file 2023.04/ndk/Dockerfile -t cimg/android:2023.04.1-ndk -t cimg/android:2023.04-ndk --platform linux/amd64 .
+docker build --file 2023.04/node/Dockerfile -t cimg/android:2023.04.1-node -t cimg/android:2023.04-node --platform linux/amd64 .
+docker build --file 2023.04/browsers/Dockerfile -t cimg/android:2023.04.1-browsers -t cimg/android:2023.04-browsers --platform linux/amd64 .
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/android:2023.03
-docker push cimg/android:2023.03.1
-docker push cimg/android:2023.03-ndk
-docker push cimg/android:2023.03.1-ndk
-docker push cimg/android:2023.03-node
-docker push cimg/android:2023.03.1-node
-docker push cimg/android:2023.03-browsers
-docker push cimg/android:2023.03.1-browsers
+docker push cimg/android:2023.04
+docker push cimg/android:2023.04.1
+docker push cimg/android:2023.04-ndk
+docker push cimg/android:2023.04.1-ndk
+docker push cimg/android:2023.04-node
+docker push cimg/android:2023.04.1-node
+docker push cimg/android:2023.04-browsers
+docker push cimg/android:2023.04.1-browsers
