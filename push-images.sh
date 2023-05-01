db90611c668d6514d69779fff774172af80eb813
ryanbourdais <ryanbourdais@outlook.com>
2023-05-01 09:27:13 -0500

    Publish v2023.05.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/android:2023.03
-docker push cimg/android:2023.03.1
-docker push cimg/android:2023.03-ndk
-docker push cimg/android:2023.03.1-ndk
-docker push cimg/android:2023.03-node
-docker push cimg/android:2023.03.1-node
-docker push cimg/android:2023.03-browsers
-docker push cimg/android:2023.03.1-browsers
+docker push cimg/android:2023.05
+docker push cimg/android:2023.05.1
+docker push cimg/android:2023.05-ndk
+docker push cimg/android:2023.05.1-ndk
+docker push cimg/android:2023.05-node
+docker push cimg/android:2023.05.1-node
+docker push cimg/android:2023.05-browsers
+docker push cimg/android:2023.05.1-browsers
