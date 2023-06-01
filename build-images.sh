f710d8d2a039f55b580ea861c37b78be20195112
Ryan Bourdais <67397011+ryanbourdais@users.noreply.github.com>
2023-06-01 13:19:55 -0500

    Publish v2023.06.1. [release] (#84)
    * Publish v2023.06.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.05/Dockerfile -t cimg/android:2023.05.1 -t cimg/android:2023.05 --platform linux/amd64 .
-docker build --file 2023.05/ndk/Dockerfile -t cimg/android:2023.05.1-ndk -t cimg/android:2023.05-ndk --platform linux/amd64 .
-docker build --file 2023.05/node/Dockerfile -t cimg/android:2023.05.1-node -t cimg/android:2023.05-node --platform linux/amd64 .
-docker build --file 2023.05/browsers/Dockerfile -t cimg/android:2023.05.1-browsers -t cimg/android:2023.05-browsers --platform linux/amd64 .
+docker build --file 2023.06/Dockerfile -t cimg/android:2023.06.1 -t cimg/android:2023.06 --platform linux/amd64 .
+docker build --file 2023.06/ndk/Dockerfile -t cimg/android:2023.06.1-ndk -t cimg/android:2023.06-ndk --platform linux/amd64 .
+docker build --file 2023.06/node/Dockerfile -t cimg/android:2023.06.1-node -t cimg/android:2023.06-node --platform linux/amd64 .
+docker build --file 2023.06/browsers/Dockerfile -t cimg/android:2023.06.1-browsers -t cimg/android:2023.06-browsers --platform linux/amd64 .
