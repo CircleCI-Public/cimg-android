85d3e5105ade08826c09507aee4552293b9ae66c
Ryan Bourdais <67397011+ryanbourdais@users.noreply.github.com>
2023-07-10 10:36:52 -0500

    Publish v2023.07. [release] (#86)

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.06/Dockerfile -t cimg/android:2023.06.1 -t cimg/android:2023.06 --platform linux/amd64 .
-docker build --file 2023.06/ndk/Dockerfile -t cimg/android:2023.06.1-ndk -t cimg/android:2023.06-ndk --platform linux/amd64 .
-docker build --file 2023.06/node/Dockerfile -t cimg/android:2023.06.1-node -t cimg/android:2023.06-node --platform linux/amd64 .
-docker build --file 2023.06/browsers/Dockerfile -t cimg/android:2023.06.1-browsers -t cimg/android:2023.06-browsers --platform linux/amd64 .
+docker build --file 2023.07/Dockerfile -t cimg/android:2023.07 -t cimg/android:2023.07 --platform linux/amd64 .
+docker build --file 2023.07/ndk/Dockerfile -t cimg/android:2023.07-ndk -t cimg/android:2023.07-ndk --platform linux/amd64 .
+docker build --file 2023.07/node/Dockerfile -t cimg/android:2023.07-node -t cimg/android:2023.07-node --platform linux/amd64 .
+docker build --file 2023.07/browsers/Dockerfile -t cimg/android:2023.07-browsers -t cimg/android:2023.07-browsers --platform linux/amd64 .
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/android:2023.06
-docker push cimg/android:2023.06.1
-docker push cimg/android:2023.06-ndk
-docker push cimg/android:2023.06.1-ndk
-docker push cimg/android:2023.06-node
-docker push cimg/android:2023.06.1-node
-docker push cimg/android:2023.06-browsers
-docker push cimg/android:2023.06.1-browsers
+docker push cimg/android:2023.07
+docker push cimg/android:2023.07-ndk
+docker push cimg/android:2023.07-node
+docker push cimg/android:2023.07-browsers
