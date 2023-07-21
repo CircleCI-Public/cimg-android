886bee004555a2df875d98f5a60a8a3c8ea8d54f
Adam Hartley <git@ahartley.com>
2023-07-21 22:14:45 +0100

    Test build

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/android:2023.07
-docker push cimg/android:2023.07-ndk
-docker push cimg/android:2023.07-node
-docker push cimg/android:2023.07-browsers
+set -eo pipefail
