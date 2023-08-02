1e054ecf412e1ca2bcba1b785417df14e56cb103
Ryan Bourdais <67397011+ryanbourdais@users.noreply.github.com>
2023-08-02 16:39:47 -0500

    Publish v2023.08.1. [release] (#90)

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/android:2023.07
-docker push cimg/android:2023.07-ndk
-docker push cimg/android:2023.07-node
-docker push cimg/android:2023.07-browsers
+docker push cimg/android:2023.08
+docker push cimg/android:2023.08.1
+docker push cimg/android:2023.08-ndk
+docker push cimg/android:2023.08.1-ndk
+docker push cimg/android:2023.08-node
+docker push cimg/android:2023.08.1-node
+docker push cimg/android:2023.08-browsers
+docker push cimg/android:2023.08.1-browsers
