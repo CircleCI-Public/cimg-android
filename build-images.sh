3c9be60949cf0d923e566fc2475270f70f5c15b4
ryanbourdais <ryanbourdais@outlook.com>
2023-10-02 10:35:47 -0500

    test: test arm builds

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 2023.07/Dockerfile -t cimg/android:2023.07 -t cimg/android:2023.07 --platform linux/amd64 .
-docker build --file 2023.07/ndk/Dockerfile -t cimg/android:2023.07-ndk -t cimg/android:2023.07-ndk --platform linux/amd64 .
-docker build --file 2023.07/node/Dockerfile -t cimg/android:2023.07-node -t cimg/android:2023.07-node --platform linux/amd64 .
-docker build --file 2023.07/browsers/Dockerfile -t cimg/android:2023.07-browsers -t cimg/android:2023.07-browsers --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.10/Dockerfile -t cimg/android:2023.10 -t cimg/android:2023.10 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.10/ndk/Dockerfile -t cimg/android:2023.10-ndk -t cimg/android:2023.10-ndk --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.10/node/Dockerfile -t cimg/android:2023.10-node -t cimg/android:2023.10-node --push .
+docker buildx build --platform=linux/amd64 --file 2023.10/browsers/Dockerfile -t cimg/android:2023.10-browsers -t cimg/android:2023.10-browsers --push .
