4846949bb1cabf7514653077d4bb5808fae37111
ryanbourdais <ryanbourdais@outlook.com>
2023-10-16 11:01:12 -0500

    Publish v2023.10.2. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.10/Dockerfile -t cimg/android:2023.10.1 -t cimg/android:2023.10 --platform linux/amd64 .
-docker build --file 2023.10/ndk/Dockerfile -t cimg/android:2023.10.1-ndk -t cimg/android:2023.10-ndk --platform linux/amd64 .
-docker build --file 2023.10/node/Dockerfile -t cimg/android:2023.10.1-node -t cimg/android:2023.10-node --platform linux/amd64 .
-docker build --file 2023.10/browsers/Dockerfile -t cimg/android:2023.10.1-browsers -t cimg/android:2023.10-browsers --platform linux/amd64 .
+docker build --file 2023.10/Dockerfile -t cimg/android:2023.10.2 -t cimg/android:2023.10 --platform linux/amd64 .
+docker build --file 2023.10/ndk/Dockerfile -t cimg/android:2023.10.2-ndk -t cimg/android:2023.10-ndk --platform linux/amd64 .
+docker build --file 2023.10/node/Dockerfile -t cimg/android:2023.10.2-node -t cimg/android:2023.10-node --platform linux/amd64 .
+docker build --file 2023.10/browsers/Dockerfile -t cimg/android:2023.10.2-browsers -t cimg/android:2023.10-browsers --platform linux/amd64 .
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/android:2023.10
-docker push cimg/android:2023.10.1
+docker push cimg/android:2023.10.2
 docker push cimg/android:2023.10-ndk
-docker push cimg/android:2023.10.1-ndk
+docker push cimg/android:2023.10.2-ndk
 docker push cimg/android:2023.10-node
-docker push cimg/android:2023.10.1-node
+docker push cimg/android:2023.10.2-node
 docker push cimg/android:2023.10-browsers
-docker push cimg/android:2023.10.1-browsers
+docker push cimg/android:2023.10.2-browsers
