e275d80924f616f3490aa0154d5ac42c8a3af830
ryanbourdais <ryanbourdais@outlook.com>
2023-11-06 16:08:37 -0600

    test

--- androidFeed.sh
+++ androidFeed.sh
@@ -12,9 +12,9 @@ else
 fi
 cd ..
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/circleci/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 
 sdkmanager --update
 
