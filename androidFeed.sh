a5379c7580dd2d38728ab454cc993ab6733d3f05
ryanbourdais <ryanbourdais@outlook.com>
2023-11-06 15:15:10 -0600

    test

--- androidFeed.sh
+++ androidFeed.sh
@@ -37,6 +37,8 @@ readarray -t BUILD_TOOLS_ARRAY <<< "$BUILD_TOOLS_VERSIONS"
 
 echo $BUILD_TOOLS_ARRAY
 
+PLATFORMS=$(sdk --list | grep "platforms;android" | cut -d'|' -f1 | grep -v 'Sandbox' | grep -v 'ext' | sort -t- -nk2 | uniq)
+echo $PLATFORMS
 
 # if [[ $CHANGES -ge 1 ]]; then
 #     generateDatedTags
