ad4742b9940c1de195bde366a041afb76981a633
ryanbourdais <ryanbourdais@outlook.com>
2023-11-06 16:38:41 -0600

    test

--- androidFeed.sh
+++ androidFeed.sh
@@ -50,6 +50,7 @@ sed -i '73c\    echo y | ${CMDLINE_TOOLS_ROOT}/sdkmanager '"${PLATFORMS_ARRAY[7]
 sed -i '78c\    sudo gem install fastlane --version '"$FASTLANE_VERSION"' --no-document && \\' Dockerfile.template
 sed -i '83c\ENV GCLOUD_VERSION='"$GCLOUD_VERSION"'' Dockerfile.template > newDockerfile.template && mv newDockerfile.template Dockerfile.template
 
-./shared/release.sh 2023.12.1
+generateDatedTags
+./shared/release.sh $RELEASE
 
 echo "yay it finished"
