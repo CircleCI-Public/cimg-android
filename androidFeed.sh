73feafb374ebfd0de3d44a4d07a4705953827c9f
ryanbourdais <ryanbourdais@outlook.com>
2023-11-06 16:20:56 -0600

    test

--- androidFeed.sh
+++ androidFeed.sh
@@ -12,10 +12,6 @@ else
 fi
 cd ..
 
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
-
 sdkmanager --update
 
 GCLOUD_VERSION=$(gcloud version | head -1 | sed 's/[^0-9.]//g')
