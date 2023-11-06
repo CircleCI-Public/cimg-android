b84481e8c43f7cc846fb317549b8e00a272b0c8f
ryanbourdais <ryanbourdais@outlook.com>
2023-11-06 13:59:45 -0600

    test

--- androidFeed.sh
+++ androidFeed.sh
@@ -28,14 +28,7 @@ echo "Maven version: "$MAVEN_VERSION
 BUILD_TOOLS=$(sdkmanager --list | grep "build-tools" | tail -1 | sed 's/.*|\(.*\)|.*/\1/' | sed 's/^[[:space:]]*//' | sed 's/ *$//' | sed -e 's/\s\+/-/g' | sed 's/ /-/g')
 echo "Build tools version: "$BUILD_TOOLS
 BUILD_TOOLS_MAJOR=$(echo $BUILD_TOOLS | awk -F'[;.]' '{print $2}')
-BUILD_TOOLS_VERSIONS=$(sdkmanager --list | grep "build-tools" |
-awk -F';' '{print $2}' |
-awk -F'|' '{print $1}' |
-sort -t. -k1,1n -k2,2n -k3,3 -k4 -s |
-awk -F. '!seen[$1"."$2"-"$3]++' | 
-sort -t. -Vr | 
-awk -F. '!seen[$1]++' |
-head -n 3)
+BUILD_TOOLS_VERSIONS=$(sdkmanager --list | grep "build-tools" | awk -F';' '{print $2}' | awk -F'|' '{print $1}' | sort -t. -k1,1n -k2,2n -k3,3 -k4 -s | awk -F. '!seen[$1"."$2"-"$3]++' | sort -t. -Vr | awk -F. '!seen[$1]++' | head -n 3)
 
 myArray=()
 
