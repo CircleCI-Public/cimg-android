e0fe0bbfcf146a02eab48977c3130ef3a373aed3
ryanbourdais <ryanbourdais@outlook.com>
2023-11-07 11:06:38 -0600

    test

--- androidFeed.sh
+++ androidFeed.sh
@@ -36,7 +36,7 @@ readarray -t BUILD_TOOLS_ARRAY <<< "$BUILD_TOOLS_VERSIONS"
 
 PLATFORMS=$(sdkmanager --list | grep "platforms;android" | cut -d'|' -f1 | grep -v 'Sandbox' | grep -v 'ext' | sort -t- -nk2 | tr -d '[:blank:]' | awk -F- '!seen[$NF]++' | tail -7)
 
-IFS=' ' read -ra <<< "$PLATFORMS"
+IFS=' ' read -ra PLATFORMS_ARRAY <<< "$PLATFORMS"
 
 echo ${PLATFORMS_ARRAY[1]}
 
