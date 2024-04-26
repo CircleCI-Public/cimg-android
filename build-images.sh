56a5ae2de8b3c0d390c31a6558279cd83f44c347
Josh Bryans <josh.bryans@circleci.com>
2024-04-26 08:46:13 -0400

    Publish v2024.04.1. [release]
    - Add --push to build-images script to confirm it is what's needed for the publish to succeed.
    - Set java21 as default

--- build-images.sh
+++ build-images.sh
@@ -2,7 +2,7 @@
 # Do not edit by hand; please use build scripts/templates to make changes
 set -eo pipefail
 
-docker build --file 2024.04/Dockerfile -t cimg/android:2024.04.1 -t cimg/android:2024.04 --platform linux/amd64 .
-docker build --file 2024.04/ndk/Dockerfile -t cimg/android:2024.04.1-ndk -t cimg/android:2024.04-ndk --platform linux/amd64 .
-docker build --file 2024.04/node/Dockerfile -t cimg/android:2024.04.1-node -t cimg/android:2024.04-node --platform linux/amd64 .
-docker build --file 2024.04/browsers/Dockerfile -t cimg/android:2024.04.1-browsers -t cimg/android:2024.04-browsers --platform linux/amd64 .
+docker build --file 2024.04/Dockerfile -t cimg/android:2024.04.1 -t cimg/android:2024.04 --platform linux/amd64 --push .
+docker build --file 2024.04/ndk/Dockerfile -t cimg/android:2024.04.1-ndk -t cimg/android:2024.04-ndk --platform linux/amd64 --push .
+docker build --file 2024.04/node/Dockerfile -t cimg/android:2024.04.1-node -t cimg/android:2024.04-node --platform linux/amd64 --push .
+docker build --file 2024.04/browsers/Dockerfile -t cimg/android:2024.04.1-browsers -t cimg/android:2024.04-browsers --platform linux/amd64 --push .
